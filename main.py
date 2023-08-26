e524bd133a49b9644de98702058ec9ff4ef2058f
kgw08003 <109195054+kgw08003@users.noreply.github.com>
2023-08-26 20:29:51 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import CTransformers
+
+# chat_model = ChatOpenAI()
+llm = CTransformers(
+    model="llama-2-7b-chat.ggmlv3.q2_K.bin",
+    model_type="llama"
+)
+
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해주세요.')
+
+if st.button('시 작성 요청하기'):
+    with st.spinner('시 작성 중...'):
+        result = llm.predict("write a poem about " + content + ": ")
+        st.write(result)
+
+# streamlit run main.py 명령어 실행
